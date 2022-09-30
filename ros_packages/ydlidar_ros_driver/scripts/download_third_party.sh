32b1775dcbf86efd67a5e3fbc6881c58e6942245
lasauveetgarde <91496274+lasauveetgarde@users.noreply.github.com>
2022-09-30 15:14:15 +0300

    Add lidar package

---
+++ ros_packages/ydlidar_ros_driver/scripts/download_third_party.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/YDLIDAR/YDLidar-SDK.git
+cd YDLidar-SDK
+mkdir build
+cd build
---
+++ ros_packages/ydlidar_ros_driver/scripts/install_third_party.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd YDLidar-SDK/build
+cmake ..
+make
+sudo make install
