73f16621f922be57ad6a83ebd92a4eaa4fcdbd72
katota <katota@gmail.com>
2022-10-19 16:18:22 +0300

    added install scripts

---
+++ scripts/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+export CMAKE_PREFIX_PATH=/usr/local/lib/cmake/:$CMAKE_PREFIX_PATH
+# export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
+
+catkin build \
+    kitty_msgs \
+    kitty_description \
+    kitty_software \
+    kitty_vision \
+    kitty_maps \
+    ackermann_raw_controller_plugin \
+    elp_stereo_camera \
+    rtabmap \
+    rtabmap_ros \
+    spatio_temporal_voxel_layer \
+    ublox \
+    cv_bridge \
+    stereo_image_proc \
+    camera_calibration \
+    maddrive_urdf_tools \
+    --cmake-args -D OpenCV_DIR="/usr/local/lib/cmake/opencv4" -D RTABMAP_SYNC_MULTI_RGBD=ON
+    
+    # serial \
+    # ti_mmwave_rospkg \
+    # viso2 \
+    # orb_slam2_ros \
---
+++ scripts/build_small.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+catkin build \
+    kitty_msgs \
+    kitty_software \
+    kitty_avr \
+    # kitty_description \
+    # kitty_maps
