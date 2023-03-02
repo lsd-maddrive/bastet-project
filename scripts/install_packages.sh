e6a819a3f4d5adb323ee754ea9a869e9f3f5583e
katota <katota@gmail.com>
2023-03-02 16:58:54 +0300

    added ukf

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-sudo apt-get install \
-    ros-$ROS_DISTRO-realsense2-camera \
-    ros-$ROS_DISTRO-usb-cam \
+sudo apt install \
     ros-$ROS_DISTRO-rosserial-server \
     ros-$ROS_DISTRO-rosserial-client \
-    ros-$ROS_DISTRO-rosserial-python \
     ros-$ROS_DISTRO-robot-localization \
     ros-$ROS_DISTRO-move-base \
     ros-$ROS_DISTRO-global-planner \
@@ -15,6 +12,9 @@ sudo apt-get install \
     ros-$ROS_DISTRO-rviz-imu-plugin \
     ros-$ROS_DISTRO-hector-gazebo-plugins \
     ros-$ROS_DISTRO-octomap-msgs \
+    ros-$ROS_DISTRO-ddynamic-reconfigure \
+    ros-$ROS_DISTRO-spatio-temporal-voxel-layer \
+    ros-$ROS_DISTRO-robot-upstart \
     libopenvdb-dev
-
+    
 #    "ros-$ROS_DISTRO-rtabmap=0.20.18-3*" \
