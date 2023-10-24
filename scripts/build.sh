15b045998ebeadee7cfbdf7a56d09bf73180f121
5met4nka <nlobazev@gmail.com>
2023-10-24 20:41:22 +0300

    deploying kitty sim

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,18 +5,25 @@ export CMAKE_PREFIX_PATH=/usr/local/lib/cmake/:$CMAKE_PREFIX_PATH
 
 catkin build \
     kitty-project \
-    rtabmap \
-    rtabmap_ros \
-    spatio_temporal_voxel_layer \
+    four_wheel_steering_controller \
+    swerve_steering_controller \
+    four_wheel_steering_msgs \
+    urdf_geometry_parser \
+    geometry2 \
+    hector_slam \
+    hector_gazebo \
     cv_bridge \
     maddrive_ros_shared \
-    realsense2_camera \
+    ydlidar_ros_driver \
     -j$(($(nproc)-2)) \
     --cmake-args \
-    -DCATKIN_ENABLE_TESTING=False \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DOpenCV_DIR="/usr/local/lib/cmake/opencv4"
+    -D CATKIN_ENABLE_TESTING=False \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D OpenCV_DIR="/usr/local/lib/cmake/opencv4"
 ##TODO - OPENCV_DIR replace like jetson nano
-    
-    
- 
+
+# catkin build \
+#     rtabmap \
+#     rtabmap_ros \
+#     realsense2_camera \
+#     cv_bridge
